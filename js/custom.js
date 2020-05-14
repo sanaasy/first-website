6d5716fdbabeadcd3fd5d73bea7918c2c9bc0bc2
Sanaa Syed <54586561+sanaasy@users.noreply.github.com>
2020-05-13 23:39:13 -0400

    used setTimeout to switch from loading screen to main page.

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,4 @@
-$(document).ready(function () {
-
-    setTimeout(function () {
-        $('body').addClass('loaded');
-        $('h1').css('color', '#222222');
-    }, 3000);
-
-});
+setTimeout(function () {
+    window.location.href = "./main.html";
+    console.log("hi")
+}, 2500);
