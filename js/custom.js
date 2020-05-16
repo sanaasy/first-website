c1de2d31ba3ba3f5af9b736f74fd6ce23dc6300e
Sanaa Syed <54586561+sanaasy@users.noreply.github.com>
2020-05-16 10:43:20 -0400

    adding a favico to the top of the  main page

--- js/custom.js
+++
@@ -1,4 +0,0 @@
-setTimeout(function () {
-    window.location.href = "./main.html";
-    console.log("hi")
-}, 2500);
---
+++ js/loading.js
@@ -0,0 +1,4 @@
+setTimeout(function () {
+    window.location.href = "./main.html";
+    console.log("hi")
+}, 2500);
